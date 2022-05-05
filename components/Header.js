34e6b94742b4eead4ab9132ecc8286cb6c1893a7
MaseCodes20 <95643884+MaseCodes20@users.noreply.github.com>
2022-05-05 16:17:30 -0400

    intial store template 1 commit

---
+++ components/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="sticky h-[40px] bg-slate-400">
+      <div className="flex justify-center items-center">
+        <button className="mx-4">
+          <h1>Logo</h1>
+        </button>
+
+        <div className="flex-1 bg-white">
+          <nav className="flex justify-center">
+            <button className="mx-2">page</button>
+            <button className="mx-2">page</button>
+            <button className="mx-2">page</button>
+            <button className="mx-2">page</button>
+          </nav>
+        </div>
+        <div className="mx-4">
+          <p>search bar</p>
+        </div>
+
+        <button className="mx-4">
+          <h1>Account logo</h1>
+        </button>
+
+        <button className="mx-4">
+          <h1>favorite icon</h1>
+        </button>
+
+        <button className="mx-4">
+          <h1>cart icon</h1>
+        </button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
