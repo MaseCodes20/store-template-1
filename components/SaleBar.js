e05dc00eb8ebea76eb15ac87869a0e09e6cfb7b6
MaseCodes20 <95643884+MaseCodes20@users.noreply.github.com>
2022-05-11 17:24:38 -0400

    salebar styling update and switch effect added

--- components/SaleBar.js
+++ components/SaleBar.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function SaleBar() {
+  const [switchDisplay, setSwitchDisplay] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSwitchDisplay(!switchDisplay);
+    }, 7000);
+  }, [switchDisplay]);
+
   return (
-    <div className="text-center h-[40px] bg-gray-300">
-      <p>Save Up to 40% off</p>
+    <div className="text-center p-8 bg-slate-500/10">
+      {switchDisplay ? (
+        <p className="font-semibold">Free shipping on Orders above $100</p>
+      ) : (
+        <p className="font-semibold">Save Up to 40% Off</p>
+      )}
     </div>
   );
 }
